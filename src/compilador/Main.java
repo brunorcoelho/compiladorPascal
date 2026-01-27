518bba0460219c385302712a8321856b825ceeae
Bruno Coelho <bruno090904@gmail.com>
2026-01-27 18:17:36 -0400

    Analisador Sintático pronto

---
+++ src/compilador/Main.java
@@ -0,0 +1,47 @@
+package compilador;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Main {
+    public static void main(String[] args) {
+        // Caminho absoluto para o arquivo de teste
+        String caminhoArquivo = "/home/bruno/Documentos/Universidade/Projeto de Compiladores/Trabalho/descricao/correto.pascal.txt";
+
+        try {
+            // Lê o conteúdo do arquivo
+            String codigoFonte = new String(Files.readAllBytes(Paths.get(caminhoArquivo)));
+
+            System.out.println("=== COMPILADOR LALG ===\n");
+            System.out.println("Arquivo: " + caminhoArquivo);
+            System.out.println("------------------------\n");
+
+            // Cria o scanner léxico
+            ScannerLexico lexer = new ScannerLexico(codigoFonte);
+
+            // Primeiro, lista todos os tokens para visualização
+            System.out.println("=== TOKENS ENCONTRADOS ===\n");
+            ScannerLexico lexerVisual = new ScannerLexico(codigoFonte);
+            TokenInformacoes token;
+            int contadorTokens = 0;
+            do {
+                token = lexerVisual.proximoToken();
+                System.out.println(token);
+                contadorTokens++;
+            } while (token.getTipo() != Token.EOF);
+
+            System.out.println("\n>>> Total de tokens: " + contadorTokens + " <<<\n");
+
+            // Agora faz análise sintática
+            System.out.println("=== ANÁLISE SINTÁTICA ===\n");
+            Parser parser = new Parser(lexer);
+            parser.parse();
+
+        } catch (IOException e) {
+            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+        }
+    }
+}
---
+++ src/compilador/Parser.java
@@ -0,0 +1,339 @@
+package compilador;
+
+public class Parser {
+    private ScannerLexico lexer;
+    private TokenInformacoes tokenAtual;
+
+    public Parser(ScannerLexico lexer) {
+        this.lexer = lexer;
+        this.tokenAtual = lexer.proximoToken();
+    }
+
+    private void avancar() {
+        tokenAtual = lexer.proximoToken();
+    }
+
+    private boolean verificar(Token tipo) {
+        return tokenAtual.getTipo() == tipo;
+    }
+
+    private void consumir(Token tipoEsperado) {
+        if (verificar(tipoEsperado)) {
+            avancar();
+        } else {
+            erro("Esperado '" + tipoEsperado + "', encontrado '" +
+                    tokenAtual.getTipo() + "' ('" + tokenAtual.getLexema() + "')");
+        }
+    }
+
+    private void erro(String mensagem) {
+        throw new RuntimeException("Erro de sintaxe na linha " + tokenAtual.getLinha() + ": " + mensagem);
+    }
+
+    public void parse() {
+        programa();
+        System.out.println("Análise sintática concluida com sucesso!");
+    }
+
+    private void programa() {
+        consumir(Token.PROGRAM);
+        consumir(Token.IDENT);
+        corpo();
+        consumir(Token.PONTO);
+    }
+
+    private void corpo() {
+        dc();
+        consumir(Token.BEGIN);
+        comandos();
+        consumir(Token.END);
+    }
+
+    private void dc() {
+        if (verificar(Token.VAR)) {
+            dc_v();
+            mais_dc();
+        } else if (verificar(Token.PROCEDURE)) {
+            dc_p();
+            mais_dc_p();
+        }
+    }
+
+    private void mais_dc() {
+        if (verificar(Token.PONTO_VIRGULA)) {
+            consumir(Token.PONTO_VIRGULA);
+            dc();
+        }
+    }
+
+    private void dc_p() {
+        consumir(Token.PROCEDURE);
+        consumir(Token.IDENT);
+        parametros();
+        corpo_p();
+    }
+
+    private void mais_dc_p() {
+        if (verificar(Token.PONTO_VIRGULA)) {
+            consumir(Token.PONTO_VIRGULA);
+            dc_p();
+            mais_dc_p();
+        }
+    }
+
+    private void parametros() {
+        if (verificar(Token.ABRE_PAREN)) {
+            consumir(Token.ABRE_PAREN);
+            lista_par();
+            consumir(Token.FECHA_PAREN);
+        }
+    }
+
+    // <lista_par> -> <variaveis> : <tipo_var> <mais_par>
+    private void lista_par() {
+        variaveis();
+        consumir(Token.DOIS_PONTOS);
+        tipo_var();
+        mais_par();
+    }
+
+    private void mais_par() {
+        if (verificar(Token.PONTO_VIRGULA)) {
+            consumir(Token.PONTO_VIRGULA);
+            lista_par();
+        }
+    }
+
+    private void corpo_p() {
+        dc_loc();
+        consumir(Token.BEGIN);
+        comandos();
+        consumir(Token.END);
+    }
+
+    private void dc_loc() {
+        if (verificar(Token.VAR)) {
+            dc_v();
+            mais_dc();
+        }
+    }
+
+    private void mais_dcloc() {
+        if (verificar(Token.PONTO_VIRGULA)) {
+            consumir(Token.PONTO_VIRGULA);
+            dc_loc();
+        }
+    }
+
+    // <dc_v> -> var <variaveis> : <tipo_var>
+    private void dc_v() {
+        consumir(Token.VAR);
+        variaveis();
+        consumir(Token.DOIS_PONTOS);
+        tipo_var();
+    }
+
+    private void tipo_var() {
+        if (verificar(Token.REAL)) {
+            consumir(Token.REAL);
+        } else if (verificar(Token.INTEGER)) {
+            consumir(Token.INTEGER);
+        } else {
+            erro("Esperado 'real' ou 'integer', encontrado '"
+                    + tokenAtual.getTipo() + "' ('" + tokenAtual.getLexema() + ")");
+        }
+    }
+
+    private void variaveis() {
+        consumir(Token.IDENT);
+        mais_var();
+    }
+
+    private void mais_var() {
+        if (verificar(Token.VIRGULA)) {
+            consumir(Token.VIRGULA);
+            variaveis();
+        }
+    }
+
+    private void comandos() {
+        comando();
+        mais_comandos();
+    }
+
+    private void mais_comandos() {
+        if (verificar(Token.READ) || verificar(Token.WRITE) || verificar(Token.IF) || verificar(Token.WHILE)
+                || verificar(Token.IDENT)) {
+            comandos();
+        }
+    }
+
+    private void comando() {
+        if (verificar(Token.READ)) {
+            consumir(Token.READ);
+            consumir(Token.ABRE_PAREN);
+            consumir(Token.IDENT);
+            consumir(Token.FECHA_PAREN);
+            consumir(Token.PONTO_VIRGULA);
+        } else if (verificar(Token.WRITE)) {
+            consumir(Token.WRITE);
+            consumir(Token.ABRE_PAREN);
+            consumir(Token.IDENT);
+            consumir(Token.FECHA_PAREN);
+            consumir(Token.PONTO_VIRGULA);
+        } else if (verificar(Token.IF)) {
+            consumir(Token.IF);
+            condicao();
+            consumir(Token.THEN);
+            comandos();
+            pfalsa();
+            consumir(Token.DOLAR);
+        } else if (verificar(Token.WHILE)) {
+            consumir(Token.WHILE);
+            condicao();
+            consumir(Token.DO);
+            comandos();
+            consumir(Token.DOLAR);
+        } else if (verificar(Token.IDENT)) {
+            consumir(Token.IDENT);
+            restoIdent();
+            consumir(Token.PONTO_VIRGULA);
+        } else {
+            erro("Comando inválido");
+        }
+    }
+
+    private void pfalsa() {
+        if (verificar(Token.ELSE)) {
+            consumir(Token.ELSE);
+            comandos();
+        }
+    }
+
+    private void restoIdent() {
+        if (verificar(Token.ATRIB)) {
+            consumir(Token.ATRIB);
+            expressao();
+        } else {
+            lista_arg();
+        }
+    }
+
+    private void lista_arg() {
+        if (verificar(Token.ABRE_PAREN)) {
+            consumir(Token.ABRE_PAREN);
+            argumentos();
+            consumir(Token.FECHA_PAREN);
+        }
+    }
+
+    private void argumentos() {
+        consumir(Token.IDENT);
+        mais_ident();
+    }
+
+    private void mais_ident() {
+        if (verificar(Token.VIRGULA)) {
+            consumir(Token.VIRGULA);
+            argumentos();
+        }
+    }
+
+    private void condicao() {
+        expressao();
+        relacao();
+        expressao();
+    }
+
+    // <relacao> -> = | <> | >= | <= | > | <
+    private void relacao() {
+        if (verificar(Token.IGUAL)) {
+            consumir(Token.IGUAL);
+        } else if (verificar(Token.DIFERENTE)) {
+            consumir(Token.DIFERENTE);
+        } else if (verificar(Token.MAIOR_IGUAL)) {
+            consumir(Token.MAIOR_IGUAL);
+        } else if (verificar(Token.MENOR_IGUAL)) {
+            consumir(Token.MENOR_IGUAL);
+        } else if (verificar(Token.MAIOR)) {
+            consumir(Token.MAIOR);
+        } else if (verificar(Token.MENOR)) {
+            consumir(Token.MENOR);
+        } else {
+            erro("Operador relacional esperado");
+        }
+    }
+
+    // <expressao> -> <termo> <outros_termos>
+    private void expressao() {
+        termo();
+        outros_termos();
+    }
+
+    // <termo> -> <op_un> <fator> <mais_fatores>
+    private void termo() {
+        op_un();
+        fator();
+        mais_fatores();
+    }
+
+    // <op_un> -> - | λ
+    private void op_un() {
+        if (verificar(Token.MENOS)) {
+            consumir(Token.MENOS);
+        }
+    }
+
+    // <fator> -> ident | numero_real | numero_int | (<expressao>)
+    private void fator() {
+        if (verificar(Token.IDENT)) {
+            consumir(Token.IDENT);
+        } else if (verificar(Token.NUMERO_REAL)) {
+            consumir(Token.NUMERO_REAL);
+        } else if (verificar(Token.NUMERO_INT)) {
+            consumir(Token.NUMERO_INT);
+        } else if (verificar(Token.ABRE_PAREN)) {
+            consumir(Token.ABRE_PAREN);
+            expressao();
+            consumir(Token.FECHA_PAREN);
+        } else {
+            erro("Fator esperado");
+        }
+    }
+
+    // <outros_termos> -> <op_ad> <termo> <outros_termos> | λ
+    private void outros_termos() {
+        if (verificar(Token.MAIS) || verificar(Token.MENOS)) {
+            op_ad();
+            termo();
+            outros_termos();
+        }
+    }
+
+    // <op_ad> -> + | -
+    private void op_ad() {
+        if (verificar(Token.MAIS)) {
+            consumir(Token.MAIS);
+        } else {
+            consumir(Token.MENOS);
+        }
+    }
+
+    // <mais_fatores> -> <op_mul> <fator> <mais_fatores> | λ
+    private void mais_fatores() {
+        if (verificar(Token.MULT) || verificar(Token.DIV)) {
+            op_mul();
+            fator();
+            mais_fatores();
+        }
+    }
+
+    // <op_mul> -> * | /
+    private void op_mul() {
+        if (verificar(Token.MULT)) {
+            consumir(Token.MULT);
+        } else {
+            consumir(Token.DIV);
+        }
+    }
+}
